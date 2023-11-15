26a0710a0944792be601f1aa50f3be4fc0feddbb
dr-frmr <docterformer@protonmail.com>
2023-11-14 20:20:59 -0500

    get rid of annoying generic context TryInto

--- src/timer.rs
+++ src/timer.rs
@@ -3,36 +3,31 @@ use anyhow::Result;
 
 /// Set a timer using the runtime that will return a Response after the specified duration.
 /// The duration should be a number of seconds.
-pub fn set_timer<T>(duration: u64, context: Option<T>) -> Result<()>
-where
-    T: TryInto<Vec<u8>, Error = anyhow::Error>,
-{
+pub fn set_timer(duration: u64, context: Option<Context>) {
     match context {
         None => {
             Request::new()
                 .target(Address::new("our", ProcessId::new("timer", "sys", "uqbar")))
                 .ipc(duration.to_le_bytes())
                 .expects_response(duration + 1)
-                .send()?;
-            Ok(())
+                .send()
+                .unwrap();
         }
         Some(context) => {
             Request::new()
                 .target(Address::new("our", ProcessId::new("timer", "sys", "uqbar")))
                 .ipc(duration.to_le_bytes())
                 .expects_response(duration + 1)
-                .try_context(context)?
-                .send()?;
-            Ok(())
+                .context(context)
+                .send()
+                .unwrap();
         }
     }
 }
 
 /// Set a timer using the runtime that will return a Response after the specified duration,
 /// then wait for that timer to resolve. The duration should be a number of seconds.
-pub fn set_and_await_timer(
-    duration: u64,
-) -> anyhow::Result<Result<(Address, Message), SendError>> {
+pub fn set_and_await_timer(duration: u64) -> anyhow::Result<Result<(Address, Message), SendError>> {
     Request::new()
         .target(Address::new("our", ProcessId::new("timer", "sys", "uqbar")))
         .ipc(duration.to_le_bytes())
