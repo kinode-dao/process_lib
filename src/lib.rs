05882b6b56126bc5dfc409b867ed62bebfb81895
dr-frmr <docterformer@protonmail.com>
2023-12-15 14:53:26 -0500

    nit

--- src/lib.rs
+++ src/lib.rs
@@ -50,7 +50,7 @@ mod message;
 pub use message::{Message, SendError, SendErrorKind};
 use message::wit_message_to_message;
 mod on_exit;
-pub use on_exit::{OnExit};
+pub use on_exit::OnExit;
 
 /// Implement the wit-bindgen specific code that the kernel uses to hook into
 /// a process. Write an `init(our: Address)` function and call it with this.
