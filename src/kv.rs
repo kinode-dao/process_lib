e83397593a44cf7e343c7f47af6a9ee0e4cf6391
bitful-pannul <bitful-pancreas@protonmail.com>
2023-12-19 13:03:24 -0300

    fix package_id imports

--- src/kv.rs
+++ src/kv.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
-
+use crate::package_id::PackageId;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct KvRequest {
--- src/sqlite.rs
+++ src/sqlite.rs
@@ -1,5 +1,7 @@
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
+use crate::package_id::PackageId;
+
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct SqliteRequest {
