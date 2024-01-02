1460ec0bca6473a298f325e1c32a3a9805732989
commercium-sys <system@commercium.systems>
2024-01-01 21:30:21 -0500

    added docstrings

--- src/eth.rs
+++ src/eth.rs
@@ -41,7 +41,7 @@ impl SubscribeLogsRequest {
 
         SubscribeLogsRequest {
             request,
-            filter: Filter::new(),
+            filter: SubscribeLogsRequest::new(),
         }
 
     }
@@ -60,56 +60,152 @@ impl SubscribeLogsRequest {
 
     }
 
+
+    /// Sets the inner filter object
+    ///
+    /// *NOTE:* ranges are always inclusive
+    ///
+    /// # Examples
+    ///
+    /// Match only a specific block
+    ///
+    /// ```rust
+    /// # use process_lib::eth::SubscribeLogsRequest;
+    /// # fn main() {
+    /// let request = SubscribeLogsRequest::new().select(69u64);
+    /// # }
+    /// ```
+    /// This is the same as `SubscribeLogsRequest::new().from_block(1337u64).to_block(1337u64)`
+    ///
+    /// Match the latest block only
+    ///
+    /// ```rust
+    /// # use process_lib::eth::{SubscribeLogsRequest, BlockNumber};
+    /// # fn main() {
+    /// let request = SubscribeLogsRequest::new().select(BlockNumber::Latest);
+    /// # }
+    /// ```
+    ///
+    /// Match a block by its hash
+    ///
+    /// ```rust
+    /// # use process_lib::eth::{SubscribeLogsRequest, H256};
+    /// # fn main() {
+    /// let request = SubscribeLogsRequest::new().select(H256::zero());
+    /// # }
+    /// ```
+    /// This is the same as `at_block_hash`
+    ///
+    /// Match a range of blocks
+    ///
+    /// ```rust
+    /// # use process_lib::eth::{SubscribeLogsRequest, H256};
+    /// # fn main() {
+    /// let request = SubscribeLogsRequest::new().select(0u64..100u64);
+    /// # }
+    /// ```
+    ///
+    /// Match all blocks in range `(1337..BlockNumber::Latest)`
+    ///
+    /// ```rust
+    /// # use process_lib::eth::{SubscribeLogsRequest, H256};
+    /// # fn main() {
+    /// let request = SubscribeLogsRequest::new().select(1337u64..);
+    /// # }
+    /// ```
+    ///
+    /// Match all blocks in range `(BlockNumber::Earliest..1337)`
+    ///
+    /// ```rust
+    /// # use process_lib::eth::{SubscribeLogsRequest, H256};
+    /// # fn main() {
+    /// let request = SubscribeLogsRequest::new().select(..1337u64);
+    /// # }
+    /// ```
     pub fn select(mut self, filter: impl Into<FilterBlockOption>) -> Self {
         self.filter = self.filter.select(filter);
         self
     }
 
-    pub fn address<T: Into<ValueOrArray<EthAddress>>>(mut self, address: T) -> Self {
-        self.filter = self.filter.address(address);
-        self
-    }
-
+    /// Matches starting from a specific block
     pub fn from_block<T: Into<BlockNumber>>(mut self, block: T) -> Self {
         self.filter = self.filter.from_block(block);
         self
     }
 
+    /// Matches up until a specific block
     pub fn to_block<T: Into<BlockNumber>>(mut self, block: T) -> Self {
         self.filter = self.filter.to_block(block);
         self
     }
 
+    /// Matches a for a specific block hash
     pub fn at_block_hash<T: Into<H256>>(mut self, hash: T) -> Self {
         self.filter = self.filter.at_block_hash(hash);
         self
     }
 
+    /// Sets the SubscribeLogs filter object
+    ///
+    /// *NOTE:* ranges are always inclusive
+    ///
+    /// # Examples
+    ///
+    /// Match only a specific address `("0xAc4b3DacB91461209Ae9d41EC517c2B9Cb1B7DAF")`
+    ///
+    /// ```rust
+    /// # use process_lib::eth::{SubscribeLogsRequest, Address};
+    /// # fn main() {
+    /// let filter = SubscribeLogsRequest::new().address("0xAc4b3DacB91461209Ae9d41EC517c2B9Cb1B7DAF".parse::<EthAddress>().unwrap());
+    /// # }
+    /// ```
+    ///
+    /// Match all addresses in array `(vec!["0xAc4b3DacB91461209Ae9d41EC517c2B9Cb1B7DAF",
+    /// "0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8"])`
+    ///
+    /// ```rust
+    /// # use process_lib::eth::{SubscribeLogsRequest, EthAddress, ValueOrArray};
+    /// # fn main() {
+    /// let addresses = vec!["0xAc4b3DacB91461209Ae9d41EC517c2B9Cb1B7DAF".parse::<EthAddress>().unwrap(),"0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8".parse::<EthAddress>().unwrap()];
+    /// let filter = SubscribeLogsRequest::new().address(addresses);
+    /// # }
+    /// ```
+    pub fn address<T: Into<ValueOrArray<EthAddress>>>(mut self, address: T) -> Self {
+        self.filter = self.filter.address(address);
+        self
+    }
+
+    /// Given the event signature in string form, it hashes it and adds it to the topics to monitor
     pub fn event(mut self, event_name: &str) -> Self {
         self.filter = self.filter.event(event_name);
         self
     }
 
+    /// Hashes all event signatures and sets them as array to topic0
     pub fn events(mut self, events: impl IntoIterator<Item = impl AsRef<[u8]>>) -> Self {
         self.filter = self.filter.events(events);
         self
     }
 
+    /// Sets topic0 (the event name for non-anonymous events)
     pub fn topic0<T: Into<Topic>>(mut self, topic: T) -> Self {
         self.filter = self.filter.topic0(topic);
         self
     }
 
+    /// Sets the 1st indexed topic
     pub fn topic1<T: Into<Topic>>(mut self, topic: T) -> Self {
         self.filter = self.filter.topic1(topic);
         self
     }
 
+    /// Sets the 2nd indexed topic
     pub fn topic2<T: Into<Topic>>(mut self, topic: T) -> Self {
         self.filter = self.filter.topic2(topic);
         self
     }
 
+    /// Sets the 3rd indexed topic
     pub fn topic3<T: Into<Topic>>(mut self, topic: T) -> Self {
         self.filter = self.filter.topic3(topic);
         self
@@ -119,6 +215,7 @@ impl SubscribeLogsRequest {
         self.filter.is_paginatable()
     }
 
+    /// Returns the numeric value of the `toBlock` field
     pub fn get_to_block(&self) -> Option<U64> {
         self.filter.get_to_block()
     }
