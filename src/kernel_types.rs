9688b60ab310691b7c8cebd940881be6434caf54
hosted-fornet <hosted-fornet@protonmail.com>
2024-01-26 23:42:05 -0800

    add python types

--- src/kernel_types.rs
+++ src/kernel_types.rs
@@ -392,3 +392,16 @@ pub fn en_wit_send_error_kind(kind: SendErrorKind) -> wit::SendErrorKind {
         SendErrorKind::Timeout => wit::SendErrorKind::Timeout,
     }
 }
+
+// python Requests and Responses encode the Request `code`
+//  and the Response `output` in the `lazy_load_blob`
+#[derive(Debug, Serialize, Deserialize)]
+pub enum PythonRequest {
+    Run,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum PythonResponse {
+    Run,
+    Err(String),
+}
