0b680dc2ccf28ffe8e7a74007864fd103d69981b
hosted-fornet <hosted-fornet@protonmail.com>
2024-01-29 21:44:56 -0800

    update http methods for extensions

--- src/http.rs
+++ src/http.rs
@@ -99,12 +99,13 @@ pub enum HttpServerAction {
         path: String,
         authenticated: bool,
         encrypted: bool,
+        extension: bool,
     },
     /// SecureBind is the same as Bind, except that it forces new connections to be made
     /// from the unique subdomain of the process that bound the path. These are *always*
     /// authenticated. Since the subdomain is unique, it will require the user to be
     /// logged in separately to the general domain authentication.
-    WebSocketSecureBind { path: String, encrypted: bool },
+    WebSocketSecureBind { path: String, encrypted: bool, extension: bool },
     /// Processes will RECEIVE this kind of request when a client connects to them.
     /// If a process does not want this websocket open, they should issue a *request*
     /// containing a [`type@HttpServerAction::WebSocketClose`] message and this channel ID.
@@ -375,7 +376,7 @@ where
 
 /// Register a WebSockets path with the HTTP server. Your app must do this
 /// in order to receive incoming WebSocket connections.
-pub fn bind_ws_path<T>(path: T, authenticated: bool, encrypted: bool) -> anyhow::Result<()>
+pub fn bind_ws_path<T>(path: T, authenticated: bool, encrypted: bool, extension: bool) -> anyhow::Result<()>
 where
     T: Into<String>,
 {
@@ -385,6 +386,7 @@ where
             path: path.into(),
             authenticated,
             encrypted,
+            extension,
         })?)
         .send_and_await_response(5)?;
     match res {
