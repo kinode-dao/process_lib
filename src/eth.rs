34cf6bda982ce2b93d8213f121fd479982f058e6
bitful-pannul <bitful-pancreas@protonmail.com>
2024-02-14 12:25:40 -0300

    eth: add docstrings

--- src/eth.rs
+++ src/eth.rs
@@ -60,7 +60,14 @@ pub enum EthError {
     RpcError(String),
 }
 
-fn send_request_and_parse_response<T: serde::de::DeserializeOwned>(
+/// Sends a request based on the specified `EthAction` and parses the response.
+///
+/// This function constructs a request targeting the Ethereum distribution system, serializes the provided `EthAction`,
+/// and sends it. It awaits a response with a specified timeout, then attempts to parse the response into the expected
+/// type `T`. This method is generic and can be used for various Ethereum actions by specifying the appropriate `EthAction`
+/// and return type `T`.
+/// Note the timeout of 5s.
+pub fn send_request_and_parse_response<T: serde::de::DeserializeOwned>(
     action: EthAction,
 ) -> anyhow::Result<T> {
     let resp = KiRequest::new()
@@ -81,6 +88,10 @@ fn send_request_and_parse_response<T: serde::de::DeserializeOwned>(
     }
 }
 
+/// Retrieves the current block number.
+///
+/// # Returns
+/// An `anyhow::Result<u64>` representing the current block number.
 pub fn get_block_number() -> anyhow::Result<u64> {
     let action = EthAction::Request {
         method: "eth_blockNumber".to_string(),
@@ -91,6 +102,14 @@ pub fn get_block_number() -> anyhow::Result<u64> {
     Ok(res.to::<u64>())
 }
 
+/// Retrieves the balance of the given address at the specified block.
+///
+/// # Parameters
+/// - `address`: The address to query the balance for.
+/// - `tag`: Optional block ID to specify the block at which the balance is queried.
+///
+/// # Returns
+/// An `anyhow::Result<U256>` representing the balance of the address.
 pub fn get_balance(address: Address, tag: Option<BlockId>) -> anyhow::Result<U256> {
     let params = serde_json::to_value((
         address,
@@ -104,6 +123,13 @@ pub fn get_balance(address: Address, tag: Option<BlockId>) -> anyhow::Result<U25
     send_request_and_parse_response::<U256>(action)
 }
 
+/// Retrieves logs based on a filter.
+///
+/// # Parameters
+/// - `filter`: The filter criteria for the logs.
+///
+/// # Returns
+/// An `anyhow::Result<Vec<Log>>` containing the logs that match the filter.
 pub fn get_logs(filter: &Filter) -> anyhow::Result<Vec<Log>> {
     let action = EthAction::Request {
         method: "eth_getLogs".to_string(),
@@ -113,6 +139,10 @@ pub fn get_logs(filter: &Filter) -> anyhow::Result<Vec<Log>> {
     send_request_and_parse_response::<Vec<Log>>(action)
 }
 
+/// Retrieves the current gas price.
+///
+/// # Returns
+/// An `anyhow::Result<U256>` representing the current gas price.
 pub fn get_gas_price() -> anyhow::Result<U256> {
     let action = EthAction::Request {
         method: "eth_gasPrice".to_string(),
@@ -122,6 +152,10 @@ pub fn get_gas_price() -> anyhow::Result<U256> {
     send_request_and_parse_response::<U256>(action)
 }
 
+/// Retrieves the chain ID.
+///
+/// # Returns
+/// An `anyhow::Result<U256>` representing the chain ID.
 pub fn get_chain_id() -> anyhow::Result<U256> {
     let action = EthAction::Request {
         method: "eth_chainId".to_string(),
@@ -131,6 +165,14 @@ pub fn get_chain_id() -> anyhow::Result<U256> {
     send_request_and_parse_response::<U256>(action)
 }
 
+/// Retrieves the number of transactions sent from the given address.
+///
+/// # Parameters
+/// - `address`: The address to query the transaction count for.
+/// - `tag`: Optional block ID to specify the block at which the count is queried.
+///
+/// # Returns
+/// An `anyhow::Result<U256>` representing the number of transactions sent from the address.
 pub fn get_transaction_count(address: Address, tag: Option<BlockId>) -> anyhow::Result<U256> {
     let params = serde_json::to_value((address, tag.unwrap_or_default()))?;
     let action = EthAction::Request {
@@ -141,6 +183,14 @@ pub fn get_transaction_count(address: Address, tag: Option<BlockId>) -> anyhow::
     send_request_and_parse_response::<U256>(action)
 }
 
+/// Retrieves a block by its hash.
+///
+/// # Parameters
+/// - `hash`: The hash of the block to retrieve.
+/// - `full_tx`: Whether to return full transaction objects or just their hashes.
+///
+/// # Returns
+/// An `anyhow::Result<Option<Block>>` representing the block, if found.
 pub fn get_block_by_hash(hash: BlockHash, full_tx: bool) -> anyhow::Result<Option<Block>> {
     let params = serde_json::to_value((hash, full_tx))?;
     let action = EthAction::Request {
@@ -150,7 +200,14 @@ pub fn get_block_by_hash(hash: BlockHash, full_tx: bool) -> anyhow::Result<Optio
 
     send_request_and_parse_response::<Option<Block>>(action)
 }
-
+/// Retrieves a block by its number or tag.
+///
+/// # Parameters
+/// - `number`: The number or tag of the block to retrieve.
+/// - `full_tx`: Whether to return full transaction objects or just their hashes.
+///
+/// # Returns
+/// An `anyhow::Result<Option<Block>>` representing the block, if found.
 pub fn get_block_by_number(
     number: BlockNumberOrTag,
     full_tx: bool,
@@ -164,6 +221,15 @@ pub fn get_block_by_number(
     send_request_and_parse_response::<Option<Block>>(action)
 }
 
+/// Retrieves the storage at a given address and key.
+///
+/// # Parameters
+/// - `address`: The address of the storage to query.
+/// - `key`: The key of the storage slot to retrieve.
+/// - `tag`: Optional block ID to specify the block at which the storage is queried.
+///
+/// # Returns
+/// An `anyhow::Result<Bytes>` representing the data stored at the given address and key.
 pub fn get_storage_at(address: Address, key: U256, tag: Option<BlockId>) -> anyhow::Result<Bytes> {
     let params = serde_json::to_value((address, key, tag.unwrap_or_default()))?;
     let action = EthAction::Request {
@@ -174,6 +240,14 @@ pub fn get_storage_at(address: Address, key: U256, tag: Option<BlockId>) -> anyh
     send_request_and_parse_response::<Bytes>(action)
 }
 
+/// Retrieves the code at a given address.
+///
+/// # Parameters
+/// - `address`: The address of the code to query.
+/// - `tag`: The block ID to specify the block at which the code is queried.
+///
+/// # Returns
+/// An `anyhow::Result<Bytes>` representing the code stored at the given address.
 pub fn get_code_at(address: Address, tag: BlockId) -> anyhow::Result<Bytes> {
     let params = serde_json::to_value((address, tag))?;
     let action = EthAction::Request {
@@ -184,6 +258,13 @@ pub fn get_code_at(address: Address, tag: BlockId) -> anyhow::Result<Bytes> {
     send_request_and_parse_response::<Bytes>(action)
 }
 
+/// Retrieves a transaction by its hash.
+///
+/// # Parameters
+/// - `hash`: The hash of the transaction to retrieve.
+///
+/// # Returns
+/// An `anyhow::Result<Option<Transaction>>` representing the transaction, if found.
 pub fn get_transaction_by_hash(hash: TxHash) -> anyhow::Result<Option<Transaction>> {
     let params = serde_json::to_value((hash,))?;
     let action = EthAction::Request {
@@ -194,6 +275,13 @@ pub fn get_transaction_by_hash(hash: TxHash) -> anyhow::Result<Option<Transactio
     send_request_and_parse_response::<Option<Transaction>>(action)
 }
 
+/// Retrieves the receipt of a transaction by its hash.
+///
+/// # Parameters
+/// - `hash`: The hash of the transaction for which the receipt is requested.
+///
+/// # Returns
+/// An `anyhow::Result<Option<TransactionReceipt>>` representing the transaction receipt, if found.
 pub fn get_transaction_receipt(hash: TxHash) -> anyhow::Result<Option<TransactionReceipt>> {
     let params = serde_json::to_value((hash,))?;
     let action = EthAction::Request {
@@ -204,6 +292,14 @@ pub fn get_transaction_receipt(hash: TxHash) -> anyhow::Result<Option<Transactio
     send_request_and_parse_response::<Option<TransactionReceipt>>(action)
 }
 
+/// Estimates the amount of gas that a transaction will consume.
+///
+/// # Parameters
+/// - `tx`: The transaction request object containing the details of the transaction to estimate gas for.
+/// - `block`: Optional block ID to specify the block at which the gas estimate should be made.
+///
+/// # Returns
+/// An `anyhow::Result<U256>` representing the estimated gas amount.
 pub fn estimate_gas(tx: TransactionRequest, block: Option<BlockId>) -> anyhow::Result<U256> {
     let params = serde_json::to_value((tx, block.unwrap_or_default()))?;
     let action = EthAction::Request {
@@ -214,7 +310,11 @@ pub fn estimate_gas(tx: TransactionRequest, block: Option<BlockId>) -> anyhow::R
     send_request_and_parse_response::<U256>(action)
 }
 
-// note will and should return empty I think...
+/// Retrieves the list of accounts controlled by the node.
+///
+/// # Returns
+/// An `anyhow::Result<Vec<Address>>` representing the list of accounts.
+/// Note: This function may return an empty list depending on the node's configuration and capabilities.
 pub fn get_accounts() -> anyhow::Result<Vec<Address>> {
     let action = EthAction::Request {
         method: "eth_accounts".to_string(),
@@ -224,6 +324,15 @@ pub fn get_accounts() -> anyhow::Result<Vec<Address>> {
     send_request_and_parse_response::<Vec<Address>>(action)
 }
 
+/// Retrieves the fee history for a given range of blocks.
+///
+/// # Parameters
+/// - `block_count`: The number of blocks to include in the history.
+/// - `last_block`: The ending block number or tag for the history range.
+/// - `reward_percentiles`: A list of percentiles to report fee rewards for.
+///
+/// # Returns
+/// An `anyhow::Result<FeeHistory>` representing the fee history for the specified range.
 pub fn get_fee_history(
     block_count: U256,
     last_block: BlockNumberOrTag,
@@ -238,6 +347,14 @@ pub fn get_fee_history(
     send_request_and_parse_response::<FeeHistory>(action)
 }
 
+/// Executes a call transaction, which is directly executed in the VM of the node, but never mined into the blockchain.
+///
+/// # Parameters
+/// - `tx`: The transaction request object containing the details of the call.
+/// - `block`: Optional block ID to specify the block at which the call should be made.
+///
+/// # Returns
+/// An `anyhow::Result<Bytes>` representing the result of the call.
 pub fn call(tx: TransactionRequest, block: Option<BlockId>) -> anyhow::Result<Bytes> {
     let params = serde_json::to_value((tx, block.unwrap_or_default()))?;
     let action = EthAction::Request {
@@ -248,6 +365,13 @@ pub fn call(tx: TransactionRequest, block: Option<BlockId>) -> anyhow::Result<By
     send_request_and_parse_response::<Bytes>(action)
 }
 
+/// Sends a raw transaction to the network.
+///
+/// # Parameters
+/// - `tx`: The raw transaction data.
+///
+/// # Returns
+/// An `anyhow::Result<TxHash>` representing the hash of the transaction once it has been sent.
 pub fn send_raw_transaction(tx: Bytes) -> anyhow::Result<TxHash> {
     let action = EthAction::Request {
         method: "eth_sendRawTransaction".to_string(),
@@ -257,8 +381,14 @@ pub fn send_raw_transaction(tx: Bytes) -> anyhow::Result<TxHash> {
     send_request_and_parse_response::<TxHash>(action)
 }
 
-/// sends requests eth_getLogs and eth_subscribe,
-/// doesn't await, handle them as incoming EthMessage::Sub, and EthResponse::Response
+/// Sends requests for `eth_getLogs` and `eth_subscribe` without waiting for a response, handling them as incoming `EthMessage::Sub` and `EthResponse::Response`.
+///
+/// # Parameters
+/// - `sub_id`: The subscription ID to be used for these operations.
+/// - `filter`: The filter criteria for the logs.
+///
+/// # Returns
+/// An `anyhow::Result<()>` indicating the operation was dispatched.
 pub fn getlogs_and_subscribe(sub_id: u64, filter: Filter) -> anyhow::Result<()> {
     let action = EthAction::SubscribeLogs {
         sub_id,
@@ -272,7 +402,14 @@ pub fn getlogs_and_subscribe(sub_id: u64, filter: Filter) -> anyhow::Result<()> 
         .send()
 }
 
-/// sends a request to eth_subscribe, doesn't wait for ok..
+/// Subscribes to logs without waiting for a confirmation.
+///
+/// # Parameters
+/// - `sub_id`: The subscription ID to be used for unsubscribing.
+/// - `filter`: The filter criteria for the logs.
+///
+/// # Returns
+/// An `anyhow::Result<()>` indicating the operation was dispatched.
 pub fn subscribe(sub_id: u64, filter: Filter) -> anyhow::Result<()> {
     let action = EthAction::SubscribeLogs {
         sub_id,
@@ -285,7 +422,13 @@ pub fn subscribe(sub_id: u64, filter: Filter) -> anyhow::Result<()> {
         .body(serde_json::to_vec(&action)?)
         .send()
 }
-
+/// Unsubscribes from a previously created subscription.
+///
+/// # Parameters
+/// - `sub_id`: The subscription ID to unsubscribe from.
+///
+/// # Returns
+/// An `anyhow::Result<()>` indicating the success or failure of the unsubscription request.
 pub fn unsubscribe(sub_id: u64) -> anyhow::Result<()> {
     let action = EthAction::UnsubscribeLogs(sub_id);
 
